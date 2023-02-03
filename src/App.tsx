4ced881c926e93f3e0f5dfe45a9d46958530509f
davron129 <normominovdavron001@gmail.com>
2023-02-03 15:47:44 +0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,16 @@ function App() {
       <div className="mapBox">
         <RegionsMap 
           width={'100%'}
-          selectedFillColor="#3B6CF8"
-          strokeLinecap="round"
-          strokeWidth="2"
           viewBox="0 0 1000 700"
           stroke="#CFCFCF"
+          strokeWidth="2"
+          strokeLinecap="round"
+          selectedFillColor="#3B6CF8"
           selectedStyle={{
             filter: 'drop-shadow(0px 34px 22px rgba(35, 67, 116, 0.25))',
             transition: '400ms linear',
           }}
-          handleClick={handleClickRegion}
+          handleClick={ handleClickRegion }
         />
         <div>
           <p>{ region && region.name }</p>
--- src/components/RegionsMap.tsx
+++ src/components/RegionsMap.tsx
@@ -1,24 +1,43 @@
 import { FC, useState, SVGProps, CSSProperties } from 'react';
-import { regionsPathArray } from './regionsArray';
+import { IRegion, regionsPathArray } from './regionsArray';
 
 interface RegionsMapProps extends SVGProps<any> {
   defaultFillColor?: string; // color for not selected region
   selectedFillColor?: string; // color for selected region
-  selectedStyle?: CSSProperties | string; // string for class name or object for inline style
+  selectedStyle?: CSSProperties// string for class name or object for inline style
   handleClick?: (regionId: string) => void // callback for click on region
 }
 
 export const RegionsMap: FC<RegionsMapProps> = ({
   defaultFillColor = '#FFFFFF',
   selectedFillColor = '#000000',
-  selectedStyle,
+  selectedStyle = {},
   handleClick,
   ...props
 }) => {
-    // state for default selected region 
+  // state for default selected region 
   const [curRegion, setCurRegion] = useState(regionsPathArray[regionsPathArray.length - 1]);
 
-  const isClass = typeof selectedStyle === 'string'; // check if selectedStyle is string for class name
+  const handleClickRegion = (region: IRegion) => {
+    setCurRegion(region);
+    if(handleClick) handleClick(region.id);
+  }
+
+  const renderRegion = (region: IRegion) => {
+    const { id, name, path } = region;
+    const isCurrentRegion = curRegion.id === region.id;
+
+    return (
+        <path
+          d={path}
+          key={id}
+          name={name}
+          onClick={ () => handleClickRegion(region) }
+          style={ isCurrentRegion ? selectedStyle : {}}
+          fill={ isCurrentRegion ? selectedFillColor : defaultFillColor}
+        />
+    )
+  }
 
   return (
     <svg
@@ -26,20 +45,7 @@ export const RegionsMap: FC<RegionsMapProps> = ({
         xmlns="http://www.w3.org/2000/svg"
         {...props}
     >
-      {regionsPathArray.map((region) => (
-        <path
-          d={region.path}
-          className={curRegion.id === region.id ? (isClass ? selectedStyle : "") : ""}
-          key={region.id}
-          name={region.name}
-          onClick={() => {
-            setCurRegion(region);
-            if(handleClick) handleClick(region.id);
-          }}
-          fill={curRegion.id === region.id ? selectedFillColor : defaultFillColor}
-          style={curRegion.id === region.id ? (!isClass ? selectedStyle : {}) : {}}
-        />
-      ))}
+      {regionsPathArray.map( renderRegion )}
     </svg>
   );
 };
